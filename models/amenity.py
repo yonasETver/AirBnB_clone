89a4d7ad5e785c23ea4e2dad23a12f323c2b77a0
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-09-04 09:25:36 +0300

    Add files via upload

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Implementation of the Amenity class
+'''
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+        Implementation for the Amenities.
+    '''
+    name = ""
